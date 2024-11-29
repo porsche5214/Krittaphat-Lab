ecf7eb0ddb41d7c046cc3dca785146eb77d4b309
Krittaphat <krittaphat_su@cmu.ac.th>
2024-11-29 16:12:51 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int i,j = 0;
+    int num ;
+    
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> num;
+    
+    while (num != 0){
+        
+        if (num % 2 == 0 ){
+           i = i+1 ;
+        }else{
+            j = j+1 ;
+        }
+        cout << "Enter an integer: ";
+        cin >> num;
+    }
+    cout << "#Even numbers = " << i <<"\n";
+    cout << "#Odd numbers = " << j <<"\n";
     return 0;
 }
